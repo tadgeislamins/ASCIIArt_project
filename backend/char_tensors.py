6c794894692e710cc2939c3dda8cbe20ee8802ae
retroflexivity <ark.shaldov@gmail.com>
2022-04-22 15:12:59 +0300

    теперь элегантно-минималистично принимает символ и возвращает тензор

--- backend/char_tensors.py
+++ backend/char_tensors.py
@@ -4,22 +4,20 @@ import torchvision
 import torchvision.transforms as transforms
 
 
-def totensor(chlist, width=30, texttoframe=1.5):
-    dict = {}
-    for ch in chlist:
-        try:
-            ch = chr(ch)
-        except TypeError:
-            pass
+def totensor(ch, width=30, texttoframe=1.5):
+    # переводим аскии-код в символ если необходимо
+    try:
+        ch = chr(ch)
+    except TypeError:
+        pass
 
-        # рисуем картинку
-        img = Image.new('RGB', (width, width * 2), (255, 255, 255))
-        d = ImageDraw.Draw(img)
-        font = ImageFont.truetype('/usr/share/fonts/truetype/dejavu/DejaVuSansMono.ttf', int(width * texttoframe))
+    # рисуем картинку
+    img = Image.new('RGB', (width, width * 2), (255, 255, 255))
+    d = ImageDraw.Draw(img)
+    font = ImageFont.truetype('/usr/share/fonts/truetype/dejavu/DejaVuSansMono.ttf', int(width * texttoframe))
 
-        d.text((width / 2, width), ch, fill=(0, 0, 0), font=font, anchor='mm')
+    d.text((width / 2, width), ch, fill=(0, 0, 0), font=font, anchor='mm')
 
-        # делаем тензором
-        tf = transforms.ToTensor()
-        dict[ch] = tf(img)
-    return dict
+    # делаем тензором
+    tf = transforms.ToTensor()
+    return tf(img)
