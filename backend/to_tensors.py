086b1326183c0f3bf53c31dd606a5fd29ee7fca5
caroline <75454672+carolineberzina@users.noreply.github.com>
2022-04-24 14:45:00 +0300

    пикча в список тензоров

---
+++ backend/to_tensors.py
@@ -0,0 +1,16 @@
+import torch
+import torchvision.transforms as T
+from PIL import Image
+import torchvision.transforms.functional as F
+
+
+def pic_to_tensors(image, ascii_w=50, tw=10):
+    image = Image.open(image)
+    transform = T.Compose([T.PILToTensor()])
+    img_t = transform(image)
+    tensor_resized = F.resize(img_t, [tw * ascii_w * 2, tw * ascii_w])
+    tlist = []
+    for long_tensors in torch.tensor_split(tensor_resized, ascii_w, dim=1):
+        tensors_in_row = torch.tensor_split(long_tensors, ascii_w, dim=2)
+        tlist.append(tensors_in_row)
+    return tlist
