1a81e48897ab4daaea963474e15a749b90b5c12c
caroline <75454672+carolineberzina@users.noreply.github.com>
2022-04-25 00:37:55 +0300

    минус лишняя переменная

--- backend/to_tensors.py
+++ backend/to_tensors.py
@@ -11,6 +11,5 @@ def pic_to_tensors(image, ascii_w=50, tw=10):
     tensor_resized = F.resize(img_t, [tw * ascii_w * 2, tw * ascii_w])
     tlist = []
     for long_tensors in torch.tensor_split(tensor_resized, ascii_w, dim=1):
-        tensors_in_row = torch.tensor_split(long_tensors, ascii_w, dim=2)
-        tlist.extend(tensors_in_row)
+        tlist.extend(torch.tensor_split(long_tensors, ascii_w, dim=2))
     return tlist
