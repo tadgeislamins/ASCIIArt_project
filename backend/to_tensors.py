da33b8495806f8eb84406cad81bf001ca2f9d3ab
caroline <75454672+carolineberzina@users.noreply.github.com>
2022-04-30 23:41:24 +0300

    теперь пятимерный

--- backend/to_tensors.py
+++ backend/to_tensors.py
@@ -9,7 +9,15 @@ def pic_to_tensors(image, ascii_w=50, tw=10, tratio=2):
     transform = T.Compose([T.PILToTensor()])
     img_t = transform(image)
     tensor_resized = F.resize(img_t, [tw * ascii_w * tratio, tw * ascii_w])
-    tlist = []
+    # for long_tensors in torch.tensor_split(tensor_resized, ascii_w, dim=1):
+    #     t = torch.stack(torch.tensor_split(long_tensors, ascii_w, dim=2), 0)
     for long_tensors in torch.tensor_split(tensor_resized, ascii_w, dim=1):
-        tlist.extend(torch.tensor_split(long_tensors, ascii_w, dim=2))
-    return tlist
+        stacked = torch.stack(torch.tensor_split(long_tensors, ascii_w, dim=2))
+    #stacked = torch.stack([torch.stack(torch.tensor_split(long_tensors,
+    # ascii_w, dim=2)) for long_tensors in torch.tensor_split(tensor_resized, ascii_w, dim=1)])
+    return stacked
+
+
+print(pic_to_tensors('fawn.jpeg').size())
+
+# Torch.size([строка, столбец, канал1, канал2, канал3])
