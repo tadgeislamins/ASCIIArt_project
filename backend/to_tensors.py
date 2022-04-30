8938116a24b84ccd20c348185008d4dbc61a6158
caroline <75454672+carolineberzina@users.noreply.github.com>
2022-05-01 00:47:15 +0300

    норм ресайз

--- backend/to_tensors.py
+++ backend/to_tensors.py
@@ -6,11 +6,17 @@ import torchvision.transforms.functional as F
 
 def pic_to_tensors(image, ascii_w=50, tw=10, tratio=2):
     image = Image.open(image)
+    size = F.get_image_size(image)
     transform = T.Compose([T.PILToTensor()])
     img_t = transform(image)
-    tensor_resized = F.resize(img_t, [tw * ascii_w * tratio, tw * ascii_w])
+    tensor_resized = F.resize(img_t, [size[1] // (tw * tratio) * (tw * tratio), tw * ascii_w])
     tlist = []
     for long_tensors in torch.tensor_split(tensor_resized, ascii_w, dim=1):
         tlist.append(torch.stack(torch.tensor_split(long_tensors, ascii_w, dim=2)))
     stacked = torch.stack(tlist)
-    return stacked
+    return stacked.size()
+
+
+pic_to_tensors('fawn.jpeg')
+
+# height - closest which works with tw * 2
