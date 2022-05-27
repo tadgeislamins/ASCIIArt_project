0d014899390dce1eb5bf716aa411e6234239f078
caroline <75454672+carolineberzina@users.noreply.github.com>
2022-05-27 23:06:24 +0300

    мультитаск?

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@ from telebot import types
 from io import BytesIO, StringIO
 from gen_shading import gen_shading
 from PIL import Image
+import pandas as pd
 
 cmd_create_shade = 'Создать тональный ASCII-арт'
 cmd_create_line = 'Создать контурный ASCII-арт'
@@ -21,19 +22,20 @@ token = open('token.txt').read()[:-1]
 bot = telebot.TeleBot(token)
 # токен бота; в телеге t.me/ASCIIArt_project_bot
 
-size = {'shade': 50, 'line': 50}
-art_type = ''
-chat = ''
+
+df = pd.DataFrame()
 
 
 # старт и кнопочки
 @bot.message_handler(commands=['start'])
 def start(message):
-    global art_type
-    art_type = 0
 
     global chat
     chat = message.from_user.id
+
+    global df
+    row = pd.DataFrame({'type': '', 'shade_size': 50, 'line_size': 50}, index=[chat])
+    df = pd.concat([df, row])
 
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
     for cmd in [cmd_create_shade, cmd_create_line, cmd_premade, cmd_goose]:
@@ -41,17 +43,16 @@ def start(message):
     bot.send_message(chat, msg_init, reply_markup=markup)
 
 
-@bot.message_handler(content_types=['text'])
+@bot.message_handler(content_types=['text'], func=lambda message: message.from_user.id in df.index)
 def functions(message):
-    global art_type
 
     if message.text == cmd_create_shade:
-        art_type = 'shade'
-        bot.send_message(chat, msg_guide_sendimg.format(size[art_type]))
+        df.at[chat, 'type'] = 'shade'
+        bot.send_message(chat, msg_guide_sendimg.format(df.at[chat, 'shade_size']))
 
     if message.text == cmd_create_line:
-        art_type = 'line'
-        bot.send_message(chat, msg_guide_sendimg.format(size[art_type]))
+        df.at[chat, 'type'] = 'line'
+        bot.send_message(chat, msg_guide_sendimg.format(df.at[chat, 'line_size']))
 
     elif message.text == cmd_premade:
         bot.send_message(chat, msg_wip)
@@ -63,24 +64,30 @@ def functions(message):
 
 
 # тут он отправляет txt файл в ответ на картинку
-@bot.message_handler(content_types=['photo'])
+@bot.message_handler(content_types=['photo'], func=lambda message: message.from_user.id in df.index)
 def send_art(message):
-    sz = size[art_type]
-    if message.caption:
-        if message.caption.isdigit():
-            sz = int(message.caption)
-        else:
-            bot.send_message(chat, msg_nan)
+    if df.at[chat, 'type'] == '':
+        bot.send_message(chat, msg_notype)
+    else:
+        if message.caption:
+            if message.caption.isdigit():
+                df.at[chat, str(df.at[chat, 'type']) + '_size'] = int(message.caption)
+            else:
+                bot.send_message(chat, msg_nan)
 
     file = bot.download_file(bot.get_file(message.photo[-1].file_id).file_path)
     img = BytesIO(file)
 
-    if art_type == 'shade':
-        bot.send_document(chat, StringIO(gen_shading(img, width=sz)), visible_file_name='art.txt')
-    elif art_type == 'line':
+    if df.at[chat, 'type'] == 'shade':
+        bot.send_document(chat, StringIO(gen_shading(img, width=df.at[chat, 'shade_size'])), visible_file_name='art.txt')
+    elif df.at[chat, 'type'] == 'line':
         bot.send_message(chat, msg_wip)
     else:
         bot.send_message(chat, msg_notype)
 
 
 bot.polling(none_stop=True, interval=0)
+
+
+if df.shape[0] >= 50:
+    df = df.drop(df.index[0])
