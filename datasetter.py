e0ed2630634299d8915b963a7b2aa24fb96482d7
retroflexivity <ark.shaldov@gmail.com>
2022-05-29 19:42:57 +0300

    пофиксил

--- datasetter.py
+++ datasetter.py
@@ -4,19 +4,6 @@ from torchvision.utils import save_image
 from os import mkdir
 from char_tensors import char_t
 
-
-# class AddGaussianNoise(object):
-#     def __init__(self, mean=0., std=1.):
-#         self.std = std
-#         self.mean = mean
-#
-#     def __call__(self, tensor):
-#         return tensor + torch.randn(tensor.size()) * self.std + self.mean
-#
-#     def __repr__(self):
-#         return self.__class__.__name__ + '(mean={0}, std={1})'.format(self.mean, self.std)
-
-
 rotate = transforms.Compose([
     transforms.RandomRotation(10, fill=255),
 ])
---
+++ extract_borders.py
@@ -0,0 +1,43 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import torch
+from torch.nn.functional import conv2d
+
+
+def sobelize(img):
+    # получаем тензор
+    img_matrix = np.array(img)
+
+    img_tensor = torch.tensor([img_matrix], dtype=torch.float)
+
+    img_tensor = img_tensor.permute(0, 3, 1, 2)
+
+    sobel_hor = [[-1, -2, -1],
+                 [ 0,  0,  0],
+                 [ 1,  2,  1]]
+
+    # одна матрица на каждый канал картинки
+    kernel = [[sobel_hor, sobel_hor, sobel_hor]]
+    kernel = torch.tensor(kernel, dtype=torch.float)
+
+    # свернём картинку с подготовленным ядром свёртки
+    img_conv_hor = conv2d(img_tensor, kernel)
+    img_conv_hor = img_conv_hor.permute(0, 2, 3, 1)
+
+    sobel_ver = [[-1, 0, 1],
+                 [-2, 0, 2],
+                 [-1, 0, 1]]
+
+    # одна матрица на каждый канал картинки
+    kernel = [[sobel_ver, sobel_ver, sobel_ver]]
+    kernel = torch.tensor(kernel, dtype=torch.float)
+
+    img_conv_ver = conv2d(img_tensor, kernel)
+
+    img_conv_ver = img_conv_ver.permute(0, 2, 3, 1)
+
+    img_conv = torch.sqrt(img_conv_ver**2 + img_conv_hor**2)
+
+    plt.figure(figsize=(1.5 * 7, 1.5 * 4))
+    plt.imshow(img_conv[0, :, :, 0])
+    plt.show()
